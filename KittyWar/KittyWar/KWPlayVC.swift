a1d503f9ef6b040db00be4dd1358016723521713
brucedsu <deisu.tech@gmail.com>
2016-10-19 16:52:50 -0700

    Updated KWPlayVC

--- KittyWar/KittyWar/KWPlayVC.swift
+++ KittyWar/KittyWar/KWPlayVC.swift
@@ -10,26 +10,10 @@ import UIKit
 
 class KWPlayVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var findGameIndicator: UIActivityIndicatorView!
+    
+    @IBAction func findGame(_ sender: UIButton) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
